073251a7bb1ba9a08c1c7aa7ed3e513ea12a5831
burt <funkingburt@gmail.com>
2009-06-24 22:37:34 +0100

    renamed

--- vendor/plugins/linky/init.rb
+++
@@ -1,8 +0,0 @@
-Autotest.add_hook :initialize do |at|
-  at.add_mapping(%r%^test/(\w\/)*\w+_test.*rb$%) { |filename, _|
-    filename
-  }
-end
-
-ActionView::Base.send :include, ZooiLinky::ViewMethods
-ActionController::Base.send :include, ZooiLinky::ControllerMethods
--- vendor/plugins/linky/test/zooi_linky_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MingeTest
-  
-  should "be true" do
-    assert true
-  end
-  
-end
