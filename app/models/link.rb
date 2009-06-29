26a6c3c1b2adea44ebca04409903288e72822d8d
burt <funkingburt@gmail.com>
2009-06-29 20:35:33 +0100

    hidden links impl part 1

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,10 @@ class Link < ActiveRecord::Base
     self.all(:conditions => conditions)
   end
   
+  def resolved?
+    @resolved
+  end
+  
   def has_children?
     !children.empty?
   end
@@ -46,19 +50,39 @@ class Link < ActiveRecord::Base
 
   private
   
+  # TODO: brent: add in option to pass a symbol
   def set_route_options
     if route.is_a? String
       @url = route
       @route_options = route_for_url(@url)
+      @resolved = true unless @route_options.nil?
     else
       @url = url_for(route)
       @route_options = route
+      @resolved = true unless @url.nil?
     end
   end
   
   def url_for(opts)
-    opts.merge!({ :only_path => true })
-    UrlWriter.new.send :url_for, opts
+    if url_resolvable_from_opts? opts
+      opts.merge!({ :only_path => true })
+      UrlWriter.new.send :url_for, opts
+    else
+      nil
+    end
+  end
+  
+  def url_resolvable_from_opts?(opts)
+    controller, action, id = opts[:controller], opts[:action], opts[:id]
+    return false if controller.nil?
+    resolvable = case action.to_sym
+      when :show, :edit, :update, :destroy
+        !id.nil?
+      when :index, :new, :create
+        true
+      else
+        false
+    end
   end
   
   def route_for_url(url, method = 'get')
---
+++ lib/zooi_linky/renderers.rb
@@ -0,0 +1,37 @@
+module ZooiLinky
+  
+  module Renderers
+    
+    # todo: brent: more work required
+    module BasicRenderer
+      
+      def links_to_tabs(links, selected_class = 'selected')
+        list = links.collect do |l|
+          if permitted_to_view_link?(l) && l.resolved?
+            content_tag :li, anchor_for_link(l), :class => (selected_class if link_selected?(l) )
+          else
+            ''
+          end
+        end
+        content_tag :ul, list
+      end
+      
+      def permitted_to_view_link?(link)
+        action_sym = link.route_options[:action].to_sym
+        resource_sym = link.route_options[:controller].gsub('/', '_').to_sym
+        permitted_to? action_sym, resource_sym
+      end
+
+      def link_selected?(link)
+        if !is_current_link?(link) && link.has_children?
+          link.children.any? { |l| is_current_link?(l) }
+        else
+          is_current_link?(link)
+        end
+      end
+      
+    end
+    
+  end
+  
+end
