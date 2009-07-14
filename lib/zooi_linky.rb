4bd3d1ea9b26bacbc2d358573a251b2be205b06d
burt <funkingburt@gmail.com>
2009-07-14 20:12:32 +0100

    more

--- lib/zooi_linky.rb
+++ lib/zooi_linky.rb
@@ -1,3 +1,12 @@
+# todo: brent: 
+# complete 
+# - back end menu
+# - front end menu
+# - permitted to view link helper
+# - breadcrumbs (for completeness)
+# - setting groups
+# - link sets
+
 module ZooiLinky
   
   def self.map(&block)
